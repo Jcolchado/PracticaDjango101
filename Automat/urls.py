3f85aa0fe96d50b9ee154d158f65e0316d393432
Jcolchado <jaime.colchado@gmail.com>
2019-11-30 12:46:13 -0600

    Se cambiaron los urls

--- Automat/urls.py
+++ Automat/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from main_app import views
+from django.urls import path, include
+from main_app import views  #Aunque este en amarillo esta Ok
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/', views.index),
+    path('',include('main_app.urls')),
+    #path('index/', views.index),
 
 ]
