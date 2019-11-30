010177c713a7dc266c30f04f2b4b38842e57796a
Jcolchado <jaime.colchado@gmail.com>
2019-11-30 10:32:05 -0600

    Primer commit

---
+++ Automat/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Automat project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Automat.settings')
+
+application = get_wsgi_application()
